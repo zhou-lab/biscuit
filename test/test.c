a7044c7b278d6712dd0649bef81e76fdf0562c01
zwdzwd <zhouwanding@gmail.com>
2016-12-21 16:57:56 -0500

    update test

--- test/test.c
+++ test/test.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-  fprintf(stdout, "testout: %1.3f\n", gsl_cdf_chisq_P(10.0,2.0));
+  fprintf(stdout, "test chisq P: %1.3f\n", gsl_cdf_chisq_P(10.0,2.0));
+  fprintf(stdout, "test chisq Q: %1.3f\n", gsl_cdf_chisq_Q(10.0,2));
   return 0;
 }
