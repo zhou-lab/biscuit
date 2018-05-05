cf771e4884bbe680cb43d1836c5c34a2b8c54af6
zwdzwd <zhouwanding@gmail.com>
2018-05-05 12:02:06 -0400

    mergecg takes -k to filter minimum depth (max across all samples)

--- src/mergecg.c
+++ src/mergecg.c
@@ -79,31 +79,35 @@ void parse_data_meth(bed1_t *b, char **fields, int nfields) {
   }
 }
 
-static int usage() {
+static int usage(int min_depth) {
 
   fprintf(stderr, "\n");
   fprintf(stderr, "Merge C annd G beta values in CpG dinucleotide context\n");
   fprintf(stderr, "Usage: biscuit mergecg <ref.fa> <in.bed>\n");
   fprintf(stderr, "Input options:\n\n");
   fprintf(stderr, "     REF       fai-indexed fasta\n");
-  fprintf(stderr, "     BED       sorted bed file, starting from column 4,5, we have alternating beta value and coverage for each sample. This is what output from biscuit vcf2bed without -e.\n");
+  fprintf(stderr, "     BED       sorted bed file, starting from column 4,5, we have alternating\n");
+  fprintf(stderr, "               beta value and coverage for each sample. This is what output from\n");
+  fprintf(stderr, "               biscuit vcf2bed without -e.\n");
   fprintf(stderr, "     -n        NOMe-seq mode, only merge C,G both in HCGD context\n");
+  fprintf(stderr, "     -k INT    minimum depth after merging (%d). This applies to the maximum depth\n", min_depth);
+  fprintf(stderr, "               across samples.\n");
   fprintf(stderr, "     -h        this help.\n");
   fprintf(stderr, "\n");
 
   return 1;
 }
 
-void format_output(bed1_t *p, char *chrm, char base_before, char base_after) {
+void format_output(bed1_t *p, char *chrm, char base_before, char base_after, int min_depth) {
   // skip reporting if all sample has zero coverage
-  int i; int allzero_cov = 1;
+  int i; int max_depth = 0;
   bed_data_meth_t *pd = (bed_data_meth_t*) p->data;
   for (i=0; i<pd->nsamples; ++i) {
-    if (pd->c_depts[i] + pd->g_depts[i] > 0) {
-      allzero_cov = 0; break;
+    if (pd->c_depts[i] + pd->g_depts[i] > max_depth) {
+      max_depth = pd->c_depts[i] + pd->g_depts[i];
     }
   }
-  if (allzero_cov) return;
+  if (max_depth == 0 || max_depth < min_depth) return;
 
   // if p is in CpG contxt, adjust the coordinates
   if (pd->ref == 'C' && base_after == 'G') p->end++;
@@ -135,16 +139,18 @@ int main_mergecg(int argc, char *argv[]) {
 
   int c;
   int nome_mode = 0;
-  while ((c = getopt(argc, argv, "nh"))>=0) {
+  int min_depth = 0;
+  while ((c = getopt(argc, argv, "nk:h"))>=0) {
     switch (c) {
-      case 'n': nome_mode=1; break;
-      case 'h': return usage(); break;
-      default: wzfatal("Unrecognized option: %c.\n", c); break;
+    case 'n': nome_mode=1; break;
+    case 'k': min_depth = atoi(optarg); break;
+    case 'h': return usage(min_depth); break;
+    default: wzfatal("Unrecognized option: %c.\n", c); break;
     }
   }
 
   if (optind + 2 > argc) { 
-    usage(); 
+    usage(min_depth); 
     wzfatal("Please supply reference file and sorted bed files.\n"); 
   }
 
@@ -183,13 +189,13 @@ int main_mergecg(int argc, char *argv[]) {
     }
 
     // report p
-    if (p->tid >= 0) format_output(p, tid2name(bed->targets, p->tid), p_base_before, p_base_after);
+    if (p->tid >= 0) format_output(p, tid2name(bed->targets, p->tid), p_base_before, p_base_after, min_depth);
     bed1_t *tmp = p; p = b; b = tmp;
     p_base_before = b_base_before; p_base_after = b_base_after;
   }
 
   // report last p
-  if (p->tid >= 0) format_output(p, tid2name(bed->targets, p->tid), p_base_before, p_base_after);
+  if (p->tid >= 0) format_output(p, tid2name(bed->targets, p->tid), p_base_before, p_base_after, min_depth);
   free_bed1(b, free_data_meth);
   free_bed1(p, free_data_meth);
   free_refcache(rc);
